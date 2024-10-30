219ed27b20ca11960719930543c941919ba39fd2
김민광 <leo2005222@gmail.com>
2024-10-30 19:43:02 +0900

    캐릭터 추가

---
+++ Platformer/Assets/Scripts/Enemy.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // 적 오브젝트의 RigidBody
+    public Rigidbody2D rigidbody2D1;
+    // 적 오브젝트의 이동속도
+    public float walk_speed;
+
+    // 적이 이동하는 경로의 제일 왼족 끝 바닥 오브젝트
+    public GameObject left_end_point;
+    // 적이 이동하는 경로의 제일 오른쪽 끝 바닥 오브젝트
+    public GameObject right_end_point;
+
+    // 적 오브젝트의 스프라이트 렌더러
+    public SpriteRenderer spriteRenderer;
+
+    // 적이 현재 왼쪽을 보고 있는지 여부
+    bool is_left;
+    // 적 이동방향(왼쪽 -1, 오른쪽 1)
+    float dir;
+
+    private void Start()
+    {
+        // 시작 시 왼쪽방향으로 설정
+        is_left = true;
+        dir = -1;
+    }
+
+    private void Update()
+    {
+        // 현재 이동 방향으로 적 오브젝트에 힘을 주어 밀기(이동)
+        rigidbody2D1.AddForce(Vector2.right * dir, ForceMode2D.Impulse);
+        // 적 이동속도 무한 가속 제한
+        LimitSpeed();
+
+        // 왼쪽으로 이동중이었고 현재 적 위치의 x값이 왼쪽 끝 오브젝트의 x값보다 작으면 이동경로의 왼쪽 끝에 도달했다는 의미
+        if (transform.position.x <= left_end_point.transform.position.x && is_left)
+        {
+            // 적 오브젝트의 속도를 0으로 만들어 멈추고
+            rigidbody2D1.velocity = Vector2.zero;
+
+            // 방향을 오른쪽으로 설정
+            is_left = false;
+            dir = 1;
+
+            // 방향에 따라 적 스프라이트 뒤집기
+            spriteRenderer.flipX = true;
+        }
+        // 오른쪽으로 이동중이었고 현재 적 위치의 x값이 오른쪽 끝 오브젝트의 x값보다 크면 이동경로의 오른쪽 끝에 도달했다는 의미
+        else if (transform.position.x >= right_end_point.transform.position.x && !is_left)
+        {
+            // 적 오브젝트의 속도를 0으로 만들어 멈추고
+            rigidbody2D1.velocity = Vector2.zero;
+
+            // 방향을 왼쪽으로 설정
+            is_left = true;
+            dir = -1;
+
+            // 방향에 따라 적 스프라이트 뒤집기
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    // 적 이동속도 무한 가속 제한
+    public void LimitSpeed()
+    {
+        // 좌우 움직이는 속도 x값이 최대 속도보다 크면
+        if (rigidbody2D1.velocity.x > walk_speed)
+        {
+            // x속도를 최대 속도 값으로 고정
+            // y속도는 기존 속도 값으로 그대로
+            rigidbody2D1.velocity = (Vector2.right * walk_speed) + (Vector2.up * rigidbody2D1.velocity.y);
+        }
+        // 왼쪽으로 움직이는 속도값은 음수로 표현
+        // 좌우 움직이는 속도 x값이 (최대 속도값 * -1) 보다 작으면
+        else if (rigidbody2D1.velocity.x < walk_speed * -1)
+        {
+            // x속도를 최대 속도 값으로 고정, 방향은 왼쪽으로
+            // y속도는 기존 속도 값으로 그대로
+            rigidbody2D1.velocity = (Vector2.left * walk_speed) + (Vector2.up * rigidbody2D1.velocity.y);
+        }
+    }
+}
--- Platformer/Assets/Scripts/GameManager.cs
+++ Platformer/Assets/Scripts/GameManager.cs
@@ -9,13 +9,26 @@ public class GameManager : MonoBehaviour
     public Text GameTimeText;
     public GameObject GameOverCanvas;
     public GameObject GameStopCanvas;
-    // Start is called before the first frame update
-    void Start()
-    {
+    public static GameManager instance;
+    public Player player;
+    // 게임 화면 내 목숨 UI 아이콘들 리스트
+    public List<Image> life = new List<Image>();
+    // 목숨이 있을 때의 이미지
+    public Sprite live_flower;
+    // 목숨이 없을 때의 이미지
+    public Sprite death_flower;
 
-    }
+    // 플레이어의 초기 위치
+    public Vector3 player_init_pos;
+    // 적 오브젝트들
+    public List<GameObject> enemies = new List<GameObject>();
+    // 적 오브젝트들의 초기 위치
+    public List<Vector3> enemy_init_pos = new List<Vector3>();
 
-    private static GameManager instance;
+    // 재시작 UI
+    public GameObject reset_window;
+    // 스테이지 성공 UI
+    public GameObject success_window;
 
     private void Awake()
     {
@@ -24,9 +37,11 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject); // Avoid having multiple instances of GameManager
             return;
         }
-
-        instance = this;
-        DontDestroyOnLoad(gameObject); // Persist across scenes
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Persist across scenes
+        }
     }
 
     // Update is called once per frame
@@ -50,6 +65,38 @@ public class GameManager : MonoBehaviour
             {
                 GameStopCanvas.SetActive(true);
                 Time.timeScale = 0f;
+            }
+        }
+    }
+    private void Start()
+    {
+        // 플레이어의 최대 목숨수 만큼 반복하여
+        for (int i = 0; i < player.max_life; i++)
+        {
+            // 목숨 이미지 보이게 설정
+            life[i].enabled = true;
+            // 목숨 이미지를 목숨이 있는 이미지로 설정
+            life[i].sprite = live_flower;
+        }
+    }
+
+    // 화면 내 목숨 UI 업데이트 함수
+    public void UpdateLife()
+    {
+        // 플레이어의 최대 목숨 수 만큼 반복
+        for (int i = 0; i < player.max_life; i++)
+        {
+            // i가 현재 목숨 수 값보다 작은 값이면
+            if (i < player.now_life)
+            {
+                // 목숨이 있는 이미지로 설정
+                life[i].sprite = live_flower;
+            }
+            // i가 현재 목숨 수 값보다 크면
+            else
+            {
+                // 목숨이 없는 이미지로 설정
+                life[i].sprite = death_flower;
             }
         }
     }
